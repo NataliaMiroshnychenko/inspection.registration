00a89b3bce703e31fc2b61a729eed12103cb777f
Natalia <natalexmir@gmail.com>
2020-04-07 12:15:32 +0300

    Initial commit

---
+++ TestCase2.js
@@ -0,0 +1,33 @@
+const Page = require("./RegistrationPage");
+const {Builder} = require('selenium-webdriver');
+const assert = require('assert');
+
+
+describe('Registration flow test', function() {
+  it('should register new user', async function() {
+    this.timeout(0);
+    let driver = await new Builder().forBrowser('chrome').build();
+    const page = new Page(driver);
+
+    try {
+      await page.load();
+      await page.enterEmail('diana.semenova.dev@gmail.com');
+      await page.enterLastName('Се');
+      await page.enterFirstName('Діана');
+      await page.enterPatronymicName('Сергіївна');
+      await page.enterPosition('Головний спеціаліст');
+      await page.enterPhone('0970000000');
+      await page.enterPassword('123456');
+      await page.selectBodytype('Центральний');
+      await page.selectAuthbody('Міністерство внутрішніх справ України');
+      await page.selectAreacontrol('Провадження охоронної діяльності, що підлягає ліцензуванню');
+      await page.verifyCaptcha();
+      await page.goNext();
+      const isRegSuccessful = await page.isRegistrationSuccessful();
+      return assert.equal(isRegSuccessful, false, 'Successful registration message is shown')
+    } finally {
+      // await driver.quit();
+    }
+    return false;
+  });
+});
